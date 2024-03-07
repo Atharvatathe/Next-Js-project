64575b74f9d7b1a931e0a0094c550bc8f008648f
AtharvaTathe <atharva.tathe55@gmail.com>
2024-03-07 17:53:42 +0530

    Working on Car hub project

---
+++ src/component/CustomButton.tsx
@@ -0,0 +1,24 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { CustomButtonProps } from "@/types";
+
+function CustomButton({
+  title,
+  containerStyle,
+  handleClick,
+  btnType,
+}: CustomButtonProps) {
+  return (
+    <button
+      disabled={false}
+      type={btnType}
+      className={`custom-btn ${containerStyle}`}
+      onClick={() => handleClick}
+    >
+      <span className={`flex-1`}>{title}</span>
+    </button>
+  );
+}
+
+export default CustomButton;
---
+++ src/component/SearchBar.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React, { useState } from "react";
+import SearchManufacturer from "./SearchManufacturer";
+
+function SearchBar() {
+  const [manufacturer, setManufacurer] = useState("");
+  const handleSearch = () => {};
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer />
+      </div>
+    </form>
+  );
+}
+
+export default SearchBar;
